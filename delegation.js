49fdaa6a464e6163132a6733880601ca47e6a66f
Eli Perelman <eli@eliperelman.com>
2012-02-03 08:48:22 -0600

    Making script more terse.

--- delegation.js
+++ delegation.js
@@ -9,53 +9,52 @@
 /*jshint curly: true, eqeqeq: true, noarg: true, undef: true, strict: true */
 
 (function ( ns ) {
-	if ( ns ) {
-		// This is the master delegate function. This method will execute every
-		// time that a delegated event is triggered (e.g. click, dblclick, etc.).
-		var delegate = function ( event /* click, dblclick, etc. */) {
-			// Cache the event and DOM element. Feature detect browsers that don't
-			// pass in the native event or target element.
-			var e = event || window.event,
-				element = e.target || e.srcElement,
-				type = e.type,
-				attrName = 'data-' + type,
-				attrValue,
-				events,
-				i,
-				l,
-				expression;
+	if ( !ns ) {
+		return;
+	}
+	
+	// This is the master delegate function. This method will execute every
+	// time that a delegated event is triggered (e.g. click, dblclick, etc.).
+	var delegate = function ( event /* click, dblclick, etc. */) {
+		// Cache the event and DOM element. Feature detect browsers that don't
+		// pass in the native event or target element.
+		var e = event || window.event,
+			element = e.target || e.srcElement,
+			type = e.type,
+			actions = ns.actions,
+			attrName = 'data-' + type,
+			attrValue,
+			events,
+			i,
+			il,
+			expression;
 
-			// Delegation works best with the bubbling phase, so we are going to 
-			// simulate that action here. Once we have the target element, walk
-			// up the DOM tree and execute its relevant actions for the current
-			// event.
-			while ( element && element.getAttribute ) {
-				if ( attrValue = element.getAttribute( attrName ) ) {
-					events = attrValue.split( ' ' );
-
-					for ( i = 0, l = events.length; i < l; i++ ) {
-						expression = ns.actions[ events[ i ] ];
-
-						// Execute the expression. If the expression explicitly returns false,
-						// quit walking up the DOM tree and exit.
-						if ( typeof expression === 'function' && expression.call( element, e ) === false ) {
-							return;
-						}
+		// Delegation works best with the bubbling phase, so we are going to 
+		// simulate that action here. Once we have the target element, walk
+		// up the DOM tree and execute its relevant actions for the current
+		// event.
+		while ( element && element.getAttribute ) {
+			if ( ( attrValue = element.getAttribute( attrName ) ) && ( events = attrValue.split( ' ' ) ).length ) {
+				for ( i = 0, il = events.length, expression = actions[ events[ i ] ]; i < il; i++ ) {
+					// Execute the expression. If the expression explicitly returns false,
+					// quit walking up the DOM tree and exit.
+					if ( typeof expression === 'function' && expression.call( element, e ) === false ) {
+						return;
 					}
 				}
-
-				// As long as execution hasn't been stopped, continue walking up the DOM tree
-				// until we eventually reach the document.
-				element = element.parentNode;
 			}
-		};
-	
-		ns.delegate = function ( eventType ) {
-			document[ 'on' + eventType ] = delegate;
-		};
 
-		ns.undelegate = function ( eventType ) {
-			delete document[ 'on' + eventType ];
-		};
-	}
+			// As long as execution hasn't been stopped, continue walking up the DOM tree
+			// until we eventually reach the document.
+			element = element.parentNode;
+		}
+	};
+
+	ns.delegate = function ( eventType ) {
+		document[ 'on' + eventType ] = delegate;
+	};
+
+	ns.undelegate = function ( eventType ) {
+		delete document[ 'on' + eventType ];
+	};
 })();
